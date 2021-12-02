cd1867ec23a8b18d21cb2f9a729f360cf54b1882
honmanyau <honmanyau@users.noreply.github.com>
2021-12-02 22:02:09 +1100

    Day 2 setup.

---
+++ src/day-2/index.ts
@@ -0,0 +1,73 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import { green } from "../utilities";
+
+// ============
+// == Config ==
+// ============
+export const DAY_NUM = 2;
+export const CHALLENGE_TITLE = "Dive!";
+
+// ==========
+// == Main ==
+// ==========
+if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === "true") {
+  const challengePathname = path.resolve(__dirname, "./input.txt");
+  const challengeFile = fs.readFileSync(challengePathname, "utf-8");
+  const input = processFile(challengeFile);
+
+  const solutionPart1 = part1Solver(input);
+  const solutionPart2 = part2Solver(input);
+
+  console.log(
+    [
+      `The solutions for Day ${DAY_NUM}: ${CHALLENGE_TITLE} are:`,
+      `  * Part 1: ${green(solutionPart1)}`,
+      `  * Part 2: ${green(solutionPart2)}`,
+    ].join("\n")
+  );
+}
+
+// ===========
+// == Types ==
+// ===========
+type InputLine = [string, number];
+type InputFile = InputLine[];
+
+// ===============
+// == Functions ==
+// ===============
+/**
+ * This function processes an example or input file into an input of a suitable
+ * format for the solver functions.
+ * @param {string} file A challenge file read in as a string.
+ * @returns {number[]} An array where each line is an entry of the challenge.
+ */
+export function processFile(file: string): InputLine[] {
+  const processLine = (line: string): InputLine => {
+    const entries = line.split(" ");
+
+    return [entries[0], Number(entries[1])];
+  };
+
+  return file.trim().split("\n").map(processLine);
+}
+
+/**
+ * The solver function for Part 1 of of the challenge.
+ * @param {number[]} input An array that represents the puzzle's input.
+ * @returns {number} The solution to Part 1 of the puzzle!
+ */
+export function part1Solver(input: InputFile): number {
+  return null;
+}
+
+/**
+ * The solver function for Part 2 of of the challenge.
+ * @param {number[]} input An array that represents the puzzle's input.
+ * @returns {number} The solution to Part 2 of the puzzle!
+ */
+export function part2Solver(input: InputFile): number {
+  return null;
+}

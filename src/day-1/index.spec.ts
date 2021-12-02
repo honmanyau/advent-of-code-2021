4690179e9a01bb531665b143a374807dd5b45e37
honmanyau <honmanyau@users.noreply.github.com>
2021-12-02 23:33:22 +1100

    Make Day 1 test format consistent because, uh, OCD.

--- src/day-1/index.spec.ts
+++ src/day-1/index.spec.ts
@@ -13,67 +13,62 @@ import {
 
 const examplePathname = path.resolve(__dirname, "./example.txt");
 const exampleFile = fs.readFileSync(examplePathname, "utf-8");
-const example = processFile(exampleFile);
+const stringifiedExample = JSON.stringify(processFile(exampleFile));
 
 // ============
 // == Part 1 ==
 // ============
-describe(`Day ${DAY_NUM}: ${CHALLENGE_TITLE} (Part 1)`, () => {
-    it(
-        [
-            `part1Solver() should return 7 for the example input:`,
-            ` [ ${example.join(" ")} ]`,
-        ].join(""),
-        () => {
+describe(`The solver for Day ${DAY_NUM}: ${CHALLENGE_TITLE}, Part 1`, () => {
+    let example: number[];
+
+    beforeEach(() => {
+        example = JSON.parse(stringifiedExample);
+    });
+
+    describe(`for the exmaple input`, () => {
+        it(`should return 7`, () => {
             const solution = part1Solver(example);
 
             assert.strictEqual(solution, 7);
-        }
-    );
+        });
+    });
 });
 
 // ============
 // == Part 2 ==
 // ============
-describe(`Day ${DAY_NUM}: ${CHALLENGE_TITLE} (Part 2)`, () => {
-    it(
-        [
-            `part2Solver() should return 5 for the example input:`,
-            ` [ ${example.join(" ")} ]`,
-        ].join(""),
-        () => {
+describe(`The solver for Day ${DAY_NUM}: ${CHALLENGE_TITLE}, Part 2`, () => {
+    let example: number[];
+
+    beforeEach(() => {
+        example = JSON.parse(stringifiedExample);
+    });
+
+    describe(`for the exmaple input`, () => {
+        it(`should return 5`, () => {
             const solution = part2Solver(example);
 
             assert.strictEqual(solution, 5);
-        }
-    );
+        });
+    });
 
-    it(
-        [
-            `part2Solver() should return 4 for the example input:`,
-            ` if the last entry is changed to 239`,
-        ].join(""),
-        () => {
-            const newExample = example.slice(0, -1).concat(239);
-            const solution = part2Solver(newExample);
+    describe(`for the exmaple input with 239 as the last entry`, () => {
+        it(`should return 4`, () => {
+            example[example.length - 1] = 239;
+
+            const solution = part2Solver(example);
 
             assert.strictEqual(solution, 4);
-        }
-    );
+        });
+    });
 
-    it(
-        [
-            `part2Solver() should return 6 for the example input:`,
-            ` if the 5th entry is changed to 201`,
-        ].join(""),
-        () => {
-            const newExample = [...example];
+    describe(`for the exmaple input with 239 as the last entry`, () => {
+        it(`should return 6`, () => {
+            example[4] = 201;
 
-            newExample[4] = 201;
-
-            const solution = part2Solver(newExample);
+            const solution = part2Solver(example);
 
             assert.strictEqual(solution, 6);
-        }
-    );
+        });
+    });
 });

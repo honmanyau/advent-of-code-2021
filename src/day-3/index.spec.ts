282f3a6c7a1baddc9e0b95c62a7a7a5675068336
honmanyau <honmanyau@users.noreply.github.com>
2021-12-04 00:20:17 +1100

    Add tests for part 1.

--- src/day-3/index.spec.ts
+++ src/day-3/index.spec.ts
@@ -27,12 +27,56 @@ describe(`The solver for Day ${DAY_NUM}: ${CHALLENGE_TITLE}, Part 1`, () => {
     });
 
     describe(`for the exmaple input`, () => {
-        it(`should return ???`, () => {
+        it(`should return return 198`, () => {
             const solution = part1Solver(example);
 
-            assert.strictEqual(solution, "");
+            assert.strictEqual(solution, 198);
         });
     });
+
+    describe(`for the exmaple input if the first line is "10100"`, () => {
+        it(`should still return return 198`, () => {
+            example[0] = "10100";
+
+            const solution = part1Solver(example);
+
+            assert.strictEqual(solution, 198);
+        });
+    });
+
+    describe(
+        [
+            `for the exmaple input if the first line is "00101"`,
+            `and the second line is "11111"`,
+        ].join(""),
+        () => {
+            it(`should still return return 184`, () => {
+                example[0] = "00101";
+                example[1] = "11111";
+
+                const solution = part1Solver(example);
+
+                assert.strictEqual(solution, 184);
+            });
+        }
+    );
+
+    describe(
+        [
+            `for the exmaple input if the second line is "01110"`,
+            `and the third line is "00110"`,
+        ].join(""),
+        () => {
+            it(`should still return return 150`, () => {
+                example[1] = "01110";
+                example[2] = "00110";
+
+                const solution = part1Solver(example);
+
+                assert.strictEqual(solution, 150);
+            });
+        }
+    );
 });
 
 // ============

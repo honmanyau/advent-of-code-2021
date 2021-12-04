63f7125c4e6a026cbe981142770d405025c545c1
honmanyau <honmanyau@users.noreply.github.com>
2021-12-04 22:35:51 +1100

    Add tests for part 1.

--- src/day-4/index.spec.ts
+++ src/day-4/index.spec.ts
@@ -4,6 +4,7 @@ import * as fs from "fs";
 import * as path from "path";
 
 import {
+    Input,
     DAY_NUM,
     CHALLENGE_TITLE,
     processFile,
@@ -19,17 +20,49 @@ const stringifiedExample = JSON.stringify(processFile(exampleFile));
 // == Part 1 ==
 // ============
 describe(`The solver for Day ${DAY_NUM}: ${CHALLENGE_TITLE}, Part 1`, () => {
-    let example: any; // Refer to Day-2's solution for a typed example.
+    let example: Input;
 
     beforeEach(() => {
         example = JSON.parse(stringifiedExample);
     });
 
+    describe(
+        [`for the exmaple input with only the first 5 numbers drawn`].join(""),
+        () => {
+            it(`should throw an error`, () => {
+                example.draws.slice(0, 5);
+
+                assert.throws(() => part1Solver(example));
+            });
+        }
+    );
+
+    describe(
+        [`for the exmaple input with only the first 11 numbers drawn`].join(""),
+        () => {
+            it(`should throw an error`, () => {
+                example.draws.slice(0, 11);
+
+                assert.throws(() => part1Solver(example));
+            });
+        }
+    );
+
     describe(`for the exmaple input`, () => {
-        it(`should return ???`, () => {
+        it(`should return 4512`, () => {
             const solution = part1Solver(example);
 
-            assert.strictEqual(solution, "");
+            assert.strictEqual(solution, 4512);
+        });
+    });
+
+    describe(`for the exmaple input with 8 as the twelfth draw`, () => {
+        it(`should return 1432`, () => {
+            example.draws[11] = "8";
+
+            const solution = part1Solver(example);
+
+            assert.strictEqual(solution, 1432);
         });
     });
 });

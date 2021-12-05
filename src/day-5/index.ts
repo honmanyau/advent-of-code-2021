8f43ec50ab8c64c4b1cd5bee020be58fa230b2c4
honmanyau <honmanyau@users.noreply.github.com>
2021-12-05 16:20:39 +1100

    🎄 Day 5 setup.

---
+++ src/day-5/index.ts
@@ -0,0 +1,63 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import { green } from "../utilities";
+
+// ============
+// == Config ==
+// ============
+export const DAY_NUM = 5;
+export const CHALLENGE_TITLE = "Hydrothermal Venture";
+
+// ==========
+// == Main ==
+// ==========
+if (process.env.SOLVE && process.env.SOLVE.toLowerCase() === "true") {
+    const challengePathname = path.resolve(__dirname, "./input.txt");
+    const challengeFile = fs.readFileSync(challengePathname, "utf-8");
+    const input = processFile(challengeFile);
+
+    const solutionPart1 = part1Solver(input);
+    const solutionPart2 = part2Solver(input);
+
+    console.log(
+        [
+            `The solutions for Day ${DAY_NUM}: ${CHALLENGE_TITLE} are:`,
+            `  * Part 1: ${green(solutionPart1)}`,
+            `  * Part 2: ${green(solutionPart2)}`,
+        ].join("\n")
+    );
+}
+
+// ===============
+// == Functions ==
+// ===============
+/**
+ * This function processes an example or input file into an input of a suitable
+ * format for the solver functions.
+ * @param {string} file A challenge file read in as a string.
+ * @returns {number[]} An array where each line is an entry of the challenge.
+ */
+export function processFile(file: string): number {
+    throw "Implement function for processing example and input files!";
+    // Example:
+    // return file.trim().split("\n").map(Number);
+}
+
+/**
+ * The solver function for part 1 of of the challenge.
+ * @param {number[]} input An array that represents the puzzle's input.
+ * @returns {number} The solution to Part 1 of the puzzle!
+ */
+export function part1Solver(input: number[]): number {
+    return null;
+}
+
+/**
+ * The solver function for prt 2 of of the challenge.
+ * @param {number[]} input An array that represents the puzzle's input.
+ * @returns {number} The solution to Part 2 of the puzzle!
+ */
+export function part2Solver(input: number[]): number {
+    return null;
+}

e1851d847ca08e519e4b0b188b1f11a8c5d1320d
honmanyau <honmanyau@users.noreply.github.com>
2021-12-31 16:00:50 +1100

    Add tests for part 1.

--- src/day-8/index.spec.ts
+++ src/day-8/index.spec.ts
@@ -4,6 +4,7 @@ import * as fs from "fs";
 import * as path from "path";
 
 import {
+    Input,
     DAY_NUM,
     CHALLENGE_TITLE,
     processFile,
@@ -26,12 +27,74 @@ describe(`The solver for Day ${DAY_NUM}: ${CHALLENGE_TITLE}, Part 1`, () => {
     });
 
     describe(`for the exmaple input`, () => {
-        it(`should return ???`, () => {
+        it(`should return 26`, () => {
             const solution = part1Solver(example);
 
-            assert.strictEqual(solution, "");
+            assert.strictEqual(solution, 26);
         });
     });
+
+    describe(`for the first entry of the exmaple input`, () => {
+        it(`should return 2`, () => {
+            const solution = part1Solver(example.slice(0, 1));
+
+            assert.strictEqual(solution, 2);
+        });
+    });
+
+    describe(`for the first 2 entries of the exmaple input`, () => {
+        it(`should return 5`, () => {
+            const solution = part1Solver(example.slice(0, 2));
+
+            assert.strictEqual(solution, 5);
+        });
+    });
+
+    describe(`for the first 3 entries of the exmaple input`, () => {
+        it(`should return 8`, () => {
+            const solution = part1Solver(example.slice(0, 3));
+
+            assert.strictEqual(solution, 8);
+        });
+    });
+
+    describe(`for the first 4 entries of the exmaple input`, () => {
+        it(`should return 9`, () => {
+            const solution = part1Solver(example.slice(0, 4));
+
+            assert.strictEqual(solution, 9);
+        });
+    });
+
+    describe(
+        [
+            `for the input be "cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb`,
+            ` fabcd edb | cefdb cefdb cefbgd cefbgd"`,
+        ].join(""),
+        () => {
+            it(`should return 0`, () => {
+                const solution = part1Solver([
+                    [
+                        [
+                            "be",
+                            "cfbegad",
+                            "cbdgef",
+                            "fgaecd",
+                            "cgeb",
+                            "fdcge",
+                            "agebfd",
+                            "fecdb",
+                            "fabcd",
+                            "edb",
+                        ],
+                        ["cefdb", "cefdb", "cefbgd", "cefbgd"],
+                    ],
+                ]);
+
+                assert.strictEqual(solution, 0);
+            });
+        }
+    );
 });
 
 // ============

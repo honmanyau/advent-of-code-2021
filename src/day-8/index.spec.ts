774150fed94cc6123861df4321ebc622dc9574fa
honmanyau <honmanyau@users.noreply.github.com>
2021-12-31 17:09:21 +1100

    Add tests for part 2.

--- src/day-8/index.spec.ts
+++ src/day-8/index.spec.ts
@@ -108,10 +108,90 @@ describe(`The solver for Day ${DAY_NUM}: ${CHALLENGE_TITLE}, Part 2`, () => {
     });
 
     describe(`for the exmaple input`, () => {
-        it(`should return ???`, () => {
+        it(`should return 61229`, () => {
             const solution = part2Solver(example);
 
-            assert.strictEqual(solution, "");
+            assert.strictEqual(solution, 61229);
+        });
+    });
+
+    describe(`for 1st entry of the exmaple input`, () => {
+        it(`should return 8394`, () => {
+            const solution = part2Solver([example[0]]);
+
+            assert.strictEqual(solution, 8394);
+        });
+    });
+
+    describe(`for 2nd entry of the exmaple input`, () => {
+        it(`should return 9781`, () => {
+            const solution = part2Solver([example[1]]);
+
+            assert.strictEqual(solution, 9781);
+        });
+    });
+
+    describe(`for 3nd entry of the exmaple input`, () => {
+        it(`should return 1197`, () => {
+            const solution = part2Solver([example[2]]);
+
+            assert.strictEqual(solution, 1197);
+        });
+    });
+
+    describe(`for 4th entry of the exmaple input`, () => {
+        it(`should return 9361`, () => {
+            const solution = part2Solver([example[3]]);
+
+            assert.strictEqual(solution, 9361);
+        });
+    });
+
+    describe(`for 5th entry of the exmaple input`, () => {
+        it(`should return 4873`, () => {
+            const solution = part2Solver([example[4]]);
+
+            assert.strictEqual(solution, 4873);
+        });
+    });
+
+    describe(`for 6th entry of the exmaple input`, () => {
+        it(`should return 8418`, () => {
+            const solution = part2Solver([example[5]]);
+
+            assert.strictEqual(solution, 8418);
+        });
+    });
+
+    describe(`for 7th entry of the exmaple input`, () => {
+        it(`should return 4548`, () => {
+            const solution = part2Solver([example[6]]);
+
+            assert.strictEqual(solution, 4548);
+        });
+    });
+
+    describe(`for 8th entry of the exmaple input`, () => {
+        it(`should return 1625`, () => {
+            const solution = part2Solver([example[7]]);
+
+            assert.strictEqual(solution, 1625);
+        });
+    });
+
+    describe(`for 9th entry of the exmaple input`, () => {
+        it(`should return 8717`, () => {
+            const solution = part2Solver([example[8]]);
+
+            assert.strictEqual(solution, 8717);
+        });
+    });
+
+    describe(`for 10th entry of the exmaple input`, () => {
+        it(`should return 4315`, () => {
+            const solution = part2Solver([example[9]]);
+
+            assert.strictEqual(solution, 4315);
         });
     });
 });

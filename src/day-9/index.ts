ff958cafa8202c8cb7566f71534e80dc2d497914
honmanyau <honmanyau@users.noreply.github.com>
2022-01-02 13:48:05 +1100

    Implement sovler for part 1.

--- src/day-9/index.ts
+++ src/day-9/index.ts
@@ -51,7 +51,43 @@ export function processFile(file: string): number[][] {
  * @returns {number} The solution to Part 1 of the puzzle!
  */
 export function part1Solver(input: number[][]): number {
-    return -1;
+    let riskLevel = 0;
+
+    for (let y = 0; y < input.length; y++) {
+        const row = input[y];
+
+        for (let x = 0; x < row.length; x++) {
+            const currentHeight = input[y][x];
+            const neighbours = [
+                [y, x - 1],
+                [y, x + 1],
+                [y - 1, x],
+                [y + 1, x],
+            ];
+            let isLowPoint = true;
+
+            for (const [adjacentY, adjacentX] of neighbours) {
+                const adjacentHeight = input[adjacentY]
+                    ? input[adjacentY][adjacentX]
+                    : undefined;
+
+                if (
+                    adjacentHeight !== undefined &&
+                    adjacentHeight <= currentHeight
+                ) {
+                    isLowPoint = false;
+
+                    break;
+                }
+            }
+
+            if (isLowPoint) {
+                riskLevel += 1 + currentHeight;
+            }
+        }
+    }
+
+    return riskLevel;
 }
 
 /**

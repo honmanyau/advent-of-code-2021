b6a4ab88d1ac158b77c1f31036346e34697b00bf
honmanyau <honmanyau@users.noreply.github.com>
2022-01-02 14:09:05 +1100

    Refactored part 1.

--- src/day-9/index.ts
+++ src/day-9/index.ts
@@ -53,6 +53,33 @@ export function processFile(file: string): number[][] {
 export function part1Solver(input: number[][]): number {
     let riskLevel = 0;
 
+    const lowPoints = findLowPoints(input);
+
+    for (const [y, x] of lowPoints) {
+        riskLevel += input[y][x] + 1;
+    }
+
+    return riskLevel;
+}
+
+/**
+ * The solver function for prt 2 of of the challenge.
+ * @param {number[]} input An array that represents the puzzle's input.
+ * @returns {number} The solution to Part 2 of the puzzle!
+ */
+export function part2Solver(input: number[][]): number {
+    return -1;
+}
+
+/**
+ * This function finds the low points in the input and returns an array of their
+ * (y, x) coordinates.
+ * @param {number[]} input An array that represents the puzzle's input.
+ * @returns {[number, number][]} An array of (y, x) coordinates.
+ */
+function findLowPoints(input: number[][]) {
+    const lowPoints: [number, number][] = [];
+
     for (let y = 0; y < input.length; y++) {
         const row = input[y];
 
@@ -82,19 +109,10 @@ export function part1Solver(input: number[][]): number {
             }
 
             if (isLowPoint) {
-                riskLevel += 1 + currentHeight;
+                lowPoints.push([y, x]);
             }
         }
     }
 
-    return riskLevel;
-}
-
-/**
- * The solver function for prt 2 of of the challenge.
- * @param {number[]} input An array that represents the puzzle's input.
- * @returns {number} The solution to Part 2 of the puzzle!
- */
-export function part2Solver(input: number[][]): number {
-    return -1;
+    return lowPoints;
 }
